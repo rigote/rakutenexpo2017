e70319b58e895d2c1796d6c8c5c6f5a7060c1266
Ricardo Ghion Fermino <rk.righion@users.noreply.github.com>
2017-08-24 13:57:56 -0300

    Provider atualizado

--- src/providers/firebase/firebase.ts
+++ src/providers/firebase/firebase.ts
@@ -1,91 +1,96 @@
-import { Injectable } from '@angular/core';
-import { Http } from '@angular/http';
-import 'rxjs/add/operator/map';
-import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database';
+import {Injectable} from '@angular/core';
+
+import firebase from 'firebase';
 
 @Injectable()
 export class FirebaseProvider {
 
-  constructor(public http: Http, public context: AngularFireDatabase) {
-  }
+    private _trilhas: any;
+    private _palestrantes: any;
+    private _palestras: any;
+    private _agendamentos: any;
+    private _patrocinadores: any;
+    private _informacoes: any;
+    public connection: any;
 
-    getPalestrante(id: any): any {
-        return this.context.list('/data/palestrantes', {
-            query: {
-                orderByChild: 'key',
-                equalTo: id 
-            }
-        });
+    constructor() {
+        this._trilhas = firebase.database().ref('/data/trilhas');
+        this._palestrantes = firebase.database().ref('/data/palestrantes');
+        this._palestras = firebase.database().ref('/data/palestras');
+        this._agendamentos = firebase.database().ref('/data/agendamentos');
+        this._patrocinadores = firebase.database().ref('/data/patrocinadores');
+        this._informacoes = firebase.database().ref('/data/informacoes');
+        this.connection = firebase.database().ref('/.info/connected');
     }
 
-  getAllPalestrantes(): any {
-      return this.context.list('/data/palestrantes');
-  }
+    public getPalestrante(id: any): any {
+        return this._palestrantes.child(id);
+    }
 
-//   getPalestra(id: any): any {
-//       return this.context.list('/data/palestras').child(id);
-//   }
+    public getAllPalestrantes(): any {
+        return this._palestrantes;
+    }
 
-  getAllPalestras(): any {
-      return this.context.list('/data/palestras', {
-        query: {
-            orderByKey: true
-        }
-    });
-  }
+    public getPalestra(id: any): any {
+        return this._palestras.child(id);
+    }
 
-//   getTrilha(id: any): any {
-//       return this.context.list('/data/trilhas').child(id);
-//   }
+    public getAllPalestras(): any {
+        return this._palestras.orderByKey();
+    }
 
-  getAllTrilhas(): any {
-      return this.context.list('/data/trilhas');
-  }
+    public getTrilha(id: any): any {
+        return this._trilhas.child(id);
+    }
 
-//   addAgendamento(item: any) {
-//       var itemsRef = this.context.list('/data/agendamentos');
-//       var newItemRef = itemsRef.push();
+    public getAllTrilhas(): any {
+        return this._trilhas;
+    }
 
-//       newItemRef.set(item);
-//   }
+    public addAgendamento(item: any) {
+        var itemsRef = this._agendamentos;
+        var newItemRef = itemsRef.push();
 
-//   updateAgendamento(id: any, item: any) {
-//       let currentItem: any = this.context.list('/data/agendamentos').child(id);
+        newItemRef.set(item);
+    }
 
-//       if (currentItem != null)
-//           currentItem.update(item)
-//   }
+    public updateAgendamento(id: any, item: any) {
+        let currentItem: any = this._agendamentos.child(id);
 
-//   removeAgendamento(id: any) {
-//     this.context.list('/data/agendamentos').remove(id);
-//   }
+        if (currentItem != null)
+            currentItem.update(item)
+    }
 
-//   getAgendamento(id: any): any {
-//       return this.context.list('/data/agendamentos').child(id);
-//   }
+    public removeAgendamento(id: any) {
+        this._agendamentos.child(id).set(null);
+    }
 
-//   getAgendamentoByUUID(uuID: any): any {
-//       return this.context.list('/data/agendamentos').orderByChild('deviceID').equalTo(uuID);
-//   }
+    public getAgendamento(id: any): any {
+        return this._agendamentos.child(id);
+    }
 
-//   getAllAgendamentos(): any {
-//       return this.context.list('/data/agendamentos');
-//   }
+    public getAgendamentoByUUID(uuID: any): any {
+        return this._agendamentos.orderByChild('deviceID').equalTo(uuID);
+    }
 
-//   getPatrocinador(id: any): any {
-//       return this.context.list('/data/patrocinadores').child(id);
-//   }
+    public getAllAgendamentos(): any {
+        return this._agendamentos;
+    }
 
-//   getAllPatrocinadores(): any {
-//       return this.context.list('/data/patrocinadores');
-//   }
+    public getPatrocinador(id: any): any {
+        return this._patrocinadores.child(id);
+    }
 
-//   getInformacao(id: any): any {
-//       return this.context.list('/data/informacoes').child(id);
-//   }
+    public getAllPatrocinadores(): any {
+        return this._patrocinadores;
+    }
 
-//   getAllInformacoes(): any {
-//       return this.context.list('/data/informacoes');
-//   }
+    public getInformacao(id: any): any {
+        return this._informacoes.child(id);
+    }
+
+    public getAllInformacoes(): any {
+        return this._informacoes;
+    }
 
 }
